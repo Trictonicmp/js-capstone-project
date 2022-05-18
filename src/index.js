2faf30024c8570700e4b829da2841a42e51b9d8e
Trictonicmp <code_shadow@hotmail.com>
2022-05-18 17:58:53 -0500

    Remove popup test button

--- src/index.js
+++ src/index.js
@@ -2,13 +2,6 @@ import './style.css';
 import { ShowPopup, enableCloseDetailsPop } from './modules/UI.js';
 import { displayHome, displayAction, displayKids } from './modules/SectionUI.js';
 
-const showDetailsButton = document.querySelector('#showDetailsButton');
-
-showDetailsButton.addEventListener('click', async () => {
-  await ShowPopup(14);
-  await enableCloseDetailsPop();
-});
-
 window.onload = () => {
   displayHome();
   document.getElementById('nav-home').onclick = () => {
