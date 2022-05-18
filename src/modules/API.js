85a6168b5d140ab5733f7f5f89e5ec95a4c3af75
Juan Rosario <jrosario19@hotmail.com>
2022-05-18 16:09:57 -0400

    Solve linters errors

--- src/modules/API.js
+++ src/modules/API.js
@@ -31,22 +31,23 @@ const getMovieComments = async (id) => {
 const getShows = async (pageNumber, showsQuantity) => {
   try {
     const response = await fetch(`https://api.tvmaze.com/shows?page=1${pageNumber}`);
-    const shows = await response.json(); 
+    const shows = await response.json();
     const returnShows = [];
-    for(let i = 0; i < showsQuantity; i += 1) {
+    for (let i = 0; i < showsQuantity; i += 1) {
       returnShows.push(shows[i]);
     }
     return returnShows;
   } catch (error) {
-    console.log('error ' + error)
+    console.log(error);
   }
-}
+  return 0;
+};
 
 const getLikes = async () => {
-  const response = await fetch(`${endPoint}/${APP_ID}/likes/`)
+  const response = await fetch(`${endPoint}/${APP_ID}/likes/`);
   const likesList = await response.json();
   return likesList;
-}
+};
 
 const addLikeTo = async (itemId) => {
   try {
@@ -55,13 +56,14 @@ const addLikeTo = async (itemId) => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ "item_id": itemId }),
+      body: JSON.stringify({ item_id: itemId }),
     });
-    return true;
-  }
-  catch(error) {
+    return response;
+  } catch (error) {
     return false;
   }
-}
+};
 
-export { getShows, getLikes, addLikeTo, getMovieById, newComment, getMovieComments };
+export {
+  getShows, getLikes, addLikeTo, getMovieById, newComment, getMovieComments,
+};
--- src/modules/Helpers.js
+++ src/modules/Helpers.js
@@ -1,9 +1,10 @@
 const getLikedOf = (itemId, likesList) => {
-  for(let i = 0; i < likesList.length; i += 1) {
-    if(likesList[i].item_id === itemId) {
+  for (let i = 0; i < likesList.length; i += 1) {
+    if (likesList[i].item_id === itemId) {
       return likesList[i].likes;
     }
   }
-}
+  return 0;
+};
 
-export { getLikedOf }
+export default getLikedOf;
